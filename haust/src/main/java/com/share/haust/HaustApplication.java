538d262a8e4decf509040a22653339db5341f223
zzk <3225483474@qq.com>
2025-03-08 21:26:04 +0800

    项目初始化

---
+++ haust/src/main/java/com/share/haust/HaustApplication.java
@@ -0,0 +1,13 @@
+package com.share.haust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaustApplication.class, args);
+    }
+
+}
---
+++ haust/src/test/java/com/share/haust/HaustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.share.haust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
