c11ba1f5f011b29ce61de451693fff53f418bc3f
zzk <3225483474@qq.com>
2025-03-09 09:35:57 +0800

    Knief4j项目文档目录完善，可以访问 localhost:8080/doc.html进行访问

--- haust/src/main/java/com/haust/HaustApplication.java
+++ haust/src/main/java/com/haust/HaustApplication.java
@@ -1,13 +1,11 @@
-package com.share.haust;
+package com.haust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HaustApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HaustApplication.class, args);
     }
-
 }
---
+++ haust/src/main/java/com/haust/configuration/SwaggerConfig.java
@@ -0,0 +1,54 @@
+package com.haust.configuration;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket user(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.haust.controller.user"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    @Bean
+    public Docket admin(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("管理端")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.haust.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo(){
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("Haust共享项目接口文档")
+                .version("1.0")
+                .description("Haust共享项目接口文档，包含用户端和管理端的所有接口")
+                .termsOfServiceUrl("https://gitcode.com/qq_43023041/HAUST_Internal_referral_code_sharing_platform")
+                .contact(contact())
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .build();
+        return apiInfo;
+    }
+    private Contact contact(){
+        return new Contact("Hauster","haust.edu.cn","web@haust.edu.cn");
+    }
+}
