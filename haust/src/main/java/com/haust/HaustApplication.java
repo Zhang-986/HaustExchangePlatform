cd663490649d26a169b16fd8e377af7afcd606d4
LoveBeryl <ljtmom414@163.com>
2025-03-09 13:17:46 +0800

    测试能否推送

--- haust/src/main/java/com/haust/HaustApplication.java
+++ haust/src/main/java/com/haust/HaustApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HaustApplication {
     public static void main(String[] args) {
+        //测试能否pull
         SpringApplication.run(HaustApplication.class, args);
     }
 }
