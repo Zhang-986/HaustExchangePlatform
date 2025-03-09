291ba4a678d4b070d6383e91a3407988f5c854b2
zzk <3225483474@qq.com>
2025-03-09 11:22:54 +0800

    异常类，常量类等完善，按照这样的情况进行编写

---
+++ haust/src/main/java/com/haust/controller/admin/LoginController.java
@@ -0,0 +1,16 @@
+package com.haust.controller.admin;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.transform.Result;
+
+/*
+管理员接口
+ */
+@RestController
+@RequestMapping("/admin")
+public class LoginController {
+
+}
