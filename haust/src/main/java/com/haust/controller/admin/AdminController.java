f5f2d842643ec255280e50e4c917199a7dc07a60
zzk <3225483474@qq.com>
2025-03-10 14:28:47 +0800

    空文件

---
+++ haust/src/main/java/com/haust/controller/admin/AdminController.java
@@ -0,0 +1,21 @@
+package com.haust.controller.admin;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+管理员接口
+ */
+
+@Api(tags = "管理员接口")
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @ApiOperation("登入接口")
+    public String login(){
+        return "asd";
+    }
+}
