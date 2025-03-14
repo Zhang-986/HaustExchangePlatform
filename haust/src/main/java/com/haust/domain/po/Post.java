42b13c7eab2d33f39f6b7937945bfe005c229ff2
zzk <3225483474@qq.com>
2025-03-14 14:43:43 +0800

    帖子，评论完成

--- haust/src/main/java/com/haust/domain/po/Post.java
+++ haust/src/main/java/com/haust/domain/po/Post.java
@@ -1,81 +1,82 @@
-package com.haust.domain.po;
+    package com.haust.domain.po;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import javax.validation.constraints.NotNull;
+    import javax.validation.constraints.NotBlank;
+    import javax.validation.constraints.Size;
+    import javax.validation.constraints.NotNull;
 
-import java.io.Serializable;
+    import java.io.Serializable;
 
-import java.time.LocalDateTime;
-import java.util.Date;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-/**
-* 帖子表
-* @TableName post
-*/
-@Builder
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Post  {
+    import java.time.LocalDateTime;
+    import java.util.Date;
+    import io.swagger.annotations.ApiModelProperty;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
 
     /**
-    * 主键，帖子的id
+    * 帖子表
+    * @TableName post
     */
-    @ApiModelProperty("主键，帖子的id")
-    private Long id;
-    /**
-    * 帖子的标题
-    */
-    @ApiModelProperty("帖子的标题")
-    private String title;
+    @Builder
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Post  {
 
-    @ApiModelProperty("帖子详细信息") private String description;
-    /**
-    * 用户id
-    */
-    @ApiModelProperty("用户id")
-    private Long userId;
-    /**
-    * 最新的一个回答的id
-    */
-    @ApiModelProperty("最新的一个回答的id")
-    private Long latestAnswerId;
-    /**
-    * 问题下的回答数量
-    */
-    @ApiModelProperty("问题下的回答数量")
-    private Integer answerTimes;
-    /**
-    * 是否匿名，默认true
-    */
-    @ApiModelProperty("是否匿名，默认true")
-    private Boolean anonymity;
-    /**
-    * 浏览次数
-    */
-    @ApiModelProperty("浏览次数")
-    private Long clickCount;
-    /**
-    * 点赞数量
-    */
-    @ApiModelProperty("点赞数量")
-    private Integer likedTimes;
-    /**
-    * 提问时间
-    */
-    @ApiModelProperty("提问时间")
-    private LocalDateTime createTime;
-    /**
-    * 更新时间
-    */
-    @ApiModelProperty("更新时间")
-    private LocalDateTime updateTime;
+        /**
+        * 主键，帖子的id
+        */
+        @ApiModelProperty("主键，帖子的id")
+        private Long id;
+        /**
+        * 帖子的标题
+        */
+        @ApiModelProperty("帖子的标题")
+        private String title;
+
+        @ApiModelProperty("帖子详细信息")
+        private String description;
+        /**
+        * 用户id
+        */
+        @ApiModelProperty("用户id")
+        private Long userId;
+        /**
+        * 最新的一个回答的id
+        */
+        @ApiModelProperty("最新的一个回答的id")
+        private Long latestAnswerId;
+        /**
+        * 问题下的回答数量
+        */
+        @ApiModelProperty("问题下的回答数量")
+        private Integer answerTimes;
+        /**
+        * 是否匿名，默认true
+        */
+        @ApiModelProperty("是否匿名，默认true")
+        private Boolean anonymity;
+        /**
+        * 浏览次数
+        */
+        @ApiModelProperty("浏览次数")
+        private Long clickCount;
+        /**
+        * 点赞数量
+        */
+        @ApiModelProperty("点赞数量")
+        private Integer likedTimes;
+        /**
+        * 提问时间
+        */
+        @ApiModelProperty("提问时间")
+        private LocalDateTime createTime;
+        /**
+        * 更新时间
+        */
+        @ApiModelProperty("更新时间")
+        private LocalDateTime updateTime;
 
 
-}
+    }
---
+++ haust/src/main/java/com/haust/domain/vo/PostVO.java
@@ -0,0 +1,41 @@
+package com.haust.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@ApiModel(description = "帖子信息返回对象")
+public class PostVO implements Serializable {
+
+    @ApiModelProperty(value = "帖子ID", example = "123")
+    private Long id;
+
+    @ApiModelProperty(value = "帖子标题", example = "如何学习Spring Boot?")
+    private String title;
+
+    @ApiModelProperty(value = "帖子详细信息", example = "求大佬指导学习路线...")
+    private String description;
+
+    @ApiModelProperty(value = "用户ID", example = "1")
+    private Long userId;
+
+    @ApiModelProperty(value = "是否匿名", example = "true")
+    private Boolean anonymity;
+
+    @ApiModelProperty(value = "浏览次数", example = "0")
+    private Long clickCount;
+
+    @ApiModelProperty(value = "点赞数量", example = "0")
+    private Integer likedTimes;
+
+    @ApiModelProperty(value = "创建时间", example = "2023-09-01 10:00:00")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "更新时间", example = "2023-09-01 10:00:00")
+    private LocalDateTime updateTime;
+
+}
