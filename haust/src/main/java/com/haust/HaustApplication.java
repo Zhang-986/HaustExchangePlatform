6566ff602c7f4f2e0d3d360049292aa13a00a5fb
gyh-gogogo <2461612633@qq.com>
2025-05-17 22:31:27 +0800

    添加了一处BUG

--- haust/src/main/java/com/haust/HaustApplication.java
+++ haust/src/main/java/com/haust/HaustApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+/**
+ * 这是GYH的PR
+ */
 @SpringBootApplication
 public class HaustApplication {
     public static void main(String[] args) {
